93f2d5b91ec457224586efb7554fdcb2c7dc15c6
Harsh Prasad <explorerharsh@gmail.com>
2021-03-23 02:15:54 +0530

    Ad free site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import MainScreen from "./screens/MainScreen";
 import FaqScreen from "./screens/FaqScreen";
 import AboutUsScreen from "./screens/AboutUsScreen";
@@ -31,7 +31,7 @@ function App() {
           <Route path="/about" component={AboutUsScreen} />
         </Container>
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </Router>
   );
 }
